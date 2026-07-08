4eae6e215f26044192a37b20eacf805751340a6c
bebomessi1 <boba47779@gmail.com>
2026-07-08 17:09:38 +0300

    Creat task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done')
+    ], default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
